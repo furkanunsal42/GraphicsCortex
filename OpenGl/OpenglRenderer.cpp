7bd27ef3864c26e6bcf5ef0b536855194d1372be
KodBilen42 <89701935+furkanunsal42@users.noreply.github.com>
2022-06-07 00:11:05 +0300

    simplification of main code

--- OpenGl/OpenglRenderer.cpp
+++ OpenGl/OpenglRenderer.cpp
@@ -7,8 +7,8 @@
 #include <vector>
 
 #include "Debuger.h"
-#include "ShaderCompiler.h"
 #include "Buffer.h";
+#include "ShaderCompiler.h"
 #include "Graphic.h"
 
 int main() {
@@ -33,15 +33,12 @@ int main() {
 	IndexBuffer index_buffer(triangles, 3);
 	Graphic square = Graphic(buffer, index_buffer);
 
-	// compile the shaders
 	Shader shader_file = read_shader("Shaders/Shader.shdr");
 	Program program(shader_file.vertex_shader, shader_file.fragment_shader);
 	
-	// access uniform to set color from cpu and transfer it to gpu's vram real-time
 	program.define_uniform("u_color");
 
-	// parameters to change color
-	float r = 0.2f;
+	float r = 0;
 	float change = 0.005f;
 	while (!glfwWindowShouldClose(window)){
 		glfwPollEvents();
@@ -49,21 +46,12 @@ int main() {
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 		
-		// update color 
 		r += change;
-		if (r > 1){ 
-			r = 1;
+		if (r > 1 || r < 0) 
 			change *= -1;
-		}
-		else if (r < 0) {
-			r = 0;
-			change *= -1;
-		}
 		
-		// send the uniform to vram
 		program.update_uniform4f("u_color", r, 0.25f + 0.5f * cos(5 * (1.6f + r * 6.28f)), 0.25f + 0.5f * sin(5 * (r * 6.28f)), 1.0f);
 		
-		// draw currently binded data using program shader
 		square.draw(program);
 
 		glfwSwapBuffers(window);
