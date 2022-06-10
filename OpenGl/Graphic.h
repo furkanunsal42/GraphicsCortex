e80b3294162cfd3e52a866e6290d6fac959058bc
KodBilen42 <89701935+furkanunsal42@users.noreply.github.com>
2022-06-10 15:42:23 +0300

    Optimized Triangularizaton

--- OpenGl/Graphic.h
+++ OpenGl/Graphic.h
@@ -18,45 +18,20 @@ public:
 		index_buffer = indicies;
 	}
 	Graphic(std::vector<float> verticies, int data_dim = 2) {
-		// calculate the mid point
-		float x_mid = 0, y_mid = 0, z_mid = 0;
-		for (int i = 0; i < verticies.size(); i+=data_dim) {
-			x_mid += verticies[i];
-			y_mid += verticies[i + 1];
-			if (data_dim == 3)
-				z_mid += verticies[i + 2];
-		}
-		x_mid /= verticies.size();
-		y_mid /= verticies.size();
-		if (data_dim == 3)
-			z_mid /= verticies.size();
-
-		// create new buffer array 
-		std::vector<float> buffer_verticies = { x_mid, y_mid };
-		if (data_dim == 3)
-			buffer_verticies.push_back(z_mid);
-		for (float vertex : verticies)
-			buffer_verticies.push_back(vertex);
-
-		// create new index array
 		std::vector<unsigned int> triangles;
-		for (int i = 1; i < verticies.size()/data_dim + 1; i++) {
+		for (int i = 1; i < verticies.size()/data_dim - 1; i++) {
+			
 			triangles.push_back(0);
 			triangles.push_back(i);
-			if(i < verticies.size() / data_dim)
-				triangles.push_back(i+1);
-			else
-				triangles.push_back(1);
-
+			triangles.push_back(i+1);
 		}
-		for (auto data : buffer_verticies)
+		for (auto data : verticies)
 			std::cout << data << " ";
 		std::cout << "\n";
 		for (auto data : triangles)
 			std::cout << data << " ";
 
-
-		ArrayBuffer array_buffer(buffer_verticies, data_dim);
+		ArrayBuffer array_buffer(verticies, data_dim);
 		IndexBuffer index_buffer(triangles, 3);
 
 		this->vertex_buffer = array_buffer;
