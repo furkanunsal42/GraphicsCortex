356c3e42686117979dda861b65acab39e24c1f40
Furkan <89701935+KodBilen42@users.noreply.github.com>
2022-08-04 08:53:24 +0300

    assimp

--- OpenGl/API/Scene.cpp
+++ OpenGl/API/Scene.cpp
@@ -10,24 +10,27 @@
 #include "Debuger.h"
 #include "Frame.h"
 
+#include "assimp/camera.h"
+
 bool once = true;
 void Scene::render(GLFWwindow* window) {
-
 
 	camera->update_matrixes();
 	AmbiantLight::count = 0;
 	DirectionalLight::count = 0;
 	PointLight::count = 0;
 	SpotLight::count = 0;
+	
+	
 	for (Light* light : lights) {
-
 		light->update_uniforms();
 	}
+
 	once = true;
 	for(Graphic* mesh : meshes){
 		// temp
 		mesh->update_matrix();
-
+		
 		mesh->renderer->update_uniform(default_program::SOLID_UNIFORM_SHORTCUTS::MODEL, mesh->model_matrix);
 		
 		if (once){
@@ -53,36 +56,7 @@ void Scene::render_to_framebuffer(GLFWwindow* window) {
 	glfwPollEvents();
 	frame::clear_window(background_color.x, background_color.y, background_color.z, background_color.w);
 
-	camera->update_matrixes();
-	AmbiantLight::count = 0;
-	DirectionalLight::count = 0;
-	PointLight::count = 0;
-	SpotLight::count = 0;
-	for (Light* light : lights) {
-
-		light->update_uniforms();
-	}
-
-	//meshes[0]->renderer->bind();
-	//meshes[0]->vertex_buffer.bind();
-	//meshes[0]->index_buffer.bind();
-	meshes[0]->material->bind();
-
-	for (Graphic* mesh : meshes) {
-		// temp
-		camera->update_uniforms(*(mesh->renderer));
-
-		mesh->update_matrix();
-		mesh->renderer->update_uniform(model_uniform_name, mesh->model_matrix);
-		mesh->renderer->update_uniform(view_uniform_name, camera->view_matrix);
-		mesh->renderer->update_uniform(projection_uniform_name, camera->projection_matrix);
-
-		mesh->renderer->update_uniform("use_color_map", (int)(mesh->material->color_map != nullptr));
-		mesh->renderer->update_uniform("use_specular_map", (int)(mesh->material->specular_map != nullptr));
-		mesh->renderer->update_uniform("use_normal_map", (int)(mesh->material->normal_map != nullptr));
-
-		mesh->draw();
-	}
+	render(window);
 
 	frame_buffer->unbind();
 	frame_buffer->render();
