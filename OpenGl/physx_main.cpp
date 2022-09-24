5e6a01b18723efc1ea3f34735ae0e7e04bc6eddb
KodBilen42 <89701935+furkanunsal42@users.noreply.github.com>
2022-09-24 06:36:29 +0300

    PhysicsScene

--- OpenGl/physx_main.cpp
+++ OpenGl/physx_main.cpp
@@ -30,36 +30,15 @@ namespace create_geometry {
 		return physx::PxCapsuleGeometry(physx::PxReal(radius), physx::PxReal(half_height));
 	}
 	
-	physx::PxConvexMeshGeometry convex_hull(const std::vector<physx::PxVec3>& verticies) {
-		
-		// verticies check
+	template<typename T>
+	std::enable_if_t<std::is_same<T, physx::PxVec3>::value || std::is_same<T, glm::vec3>::value, physx::PxConvexMeshGeometry>
+	convex_hull(const T verticies[], unsigned int count) {
 
 		physx::PxConvexMeshDesc mesh_desc;
 		physx::PxBoundedData points;
-		points.data = &(verticies[0]);
+		points.data = verticies;
 		points.stride = sizeof verticies[0];
-		points.count = 5;
-		mesh_desc.points = points;
-		mesh_desc.flags = physx::PxConvexFlag::eCOMPUTE_CONVEX;
-
-		physx::PxDefaultMemoryOutputStream buf;
-		physx::PxConvexMeshCookingResult::Enum result;
-		physics_cooking->cookConvexMesh(mesh_desc, buf, &result);
-		physx::PxDefaultMemoryInputData input(buf.getData(), buf.getSize());
-		physx::PxConvexMesh* convex_mesh = physics->createConvexMesh(input);
-		
-		return physx::PxConvexMeshGeometry(convex_mesh);
-	}
-
-	physx::PxConvexMeshGeometry convex_hull(const std::vector<glm::vec3>& verticies) {
-
-		// verticies check
-
-		physx::PxConvexMeshDesc mesh_desc;
-		physx::PxBoundedData points;
-		points.data = &(verticies[0]);
-		points.stride = sizeof verticies[0];
-		points.count = 5;
+		points.count = count;
 		mesh_desc.points = points;
 		mesh_desc.flags = physx::PxConvexFlag::eCOMPUTE_CONVEX;
 
@@ -71,9 +50,12 @@ namespace create_geometry {
 
 		return physx::PxConvexMeshGeometry(convex_mesh);
 	}
-	
 
-
+	template<typename T>
+	std::enable_if_t<std::is_same<T, physx::PxVec3>::value || std::is_same<T, glm::vec3>::value, physx::PxConvexMeshGeometry>
+	convex_hull(const std::vector<T>& verticies) {
+		return convex_hull(&(verticies[0]), verticies.size());
+	}
 }
 
 class PhysicsObject {
@@ -84,8 +66,7 @@ public:
 	physx::PxRigidDynamic* dynamic_actor;
 	bool exclusive_shape = false;
 
-	glm::vec3 position;
-	glm::vec3 rotation;
+	
 
 	PhysicsObject(const physx::PxGeometry& geometry, bool exclusive_shape = false) :
 		position(glm::vec3(0, 0, 0)), rotation(glm::vec3(0, 0, 0)), exclusive_shape(exclusive_shape)
@@ -102,6 +83,35 @@ public:
 		dynamic_actor->attachShape(*shape);
 	}
 
+	void set_position(float x, float y, float z) {
+		position.x = x;
+		position.y = y;
+		position.z = z;
+
+		update_transform();
+	}
+
+	template<typename T>
+	std::enable_if_t<std::is_same<T, glm::vec3>::value || std::is_same<T, physx::PxVec3>::value, void>
+		set_position(T rotation_vector) {
+		set_position(rotation_vector.x, rotation_vector.y, rotation_vector.z);
+	}
+
+	void set_rotation(float x, float y, float z) {
+		rotation.x = x;
+		rotation.y = y;
+		rotation.z = z;
+
+		update_transform();
+	}
+	
+	template<typename T>
+	std::enable_if_t<std::is_same<T, glm::vec3>::value || std::is_same<T, physx::PxVec3>::value, void>
+	set_rotatoin(T rotation_vector) {
+		set_rotation(rotation_vector.x, rotation_vector.y, rotation_vector.z);
+	}
+
+
 	void update_transform() {
 		glm::quat rotatoion_quat_glm(glm::vec3(rotation.x, rotation.y, rotation.z));
 		physx::PxQuat rotation_quat(rotatoion_quat_glm.x, rotatoion_quat_glm.y, rotatoion_quat_glm.z, rotatoion_quat_glm.w);
@@ -110,6 +120,10 @@ public:
 		if (dynamic_actor != nullptr)
 			dynamic_actor->setGlobalPose(transform);
 	}
+
+private:
+	glm::vec3 position;
+	glm::vec3 rotation;
 };
 
 void physics_init() {
@@ -139,46 +153,60 @@ void physics_init() {
 	physics_cooking = PxCreateCooking(PX_PHYSICS_VERSION, *physics_foundation, physx::PxCookingParams(physics->getTolerancesScale()));
 }
 
+class PhysicsScene{
+public:
+
+	std::vector<std::reference_wrapper<PhysicsObject>> actors;
+	
+	void add_actor(PhysicsObject& actor) {
+		physics_scene->addActor(*actor.dynamic_actor);
+		actors.push_back(actor);
+	}
+	
+	void simulate_step(long double delta_time) {
+		simulation_step_start(delta_time);
+		simulation_step_finish();
+	}
+
+	void simulation_step_start(long double delta_time) {
+		physics_scene->simulate(delta_time);
+	}
+
+	void simulation_step_finish() {
+		physics_scene->fetchResults(true);
+	}
+};
+
 int main() {
 	physics_init();
 	
+	PhysicsScene scene;
+
 	PhysicsObject box(create_geometry::box(1.0f, 1.0f, 1.0f), true);
-	box.rotation.x = 0.5f;
-	box.position.y = 10.0f;
-	box.update_transform();
-	physics_scene->addActor(*box.dynamic_actor);
+	scene.add_actor(box);
+	box.set_position(0.5f, 10.0f, 0.0f);
 
 	PhysicsObject sphere(create_geometry::sphere(1.0f), true);
-	sphere.position.y = 10.0f;
-	sphere.position.z = 10.0f;
-	sphere.update_transform();
-	physics_scene->addActor(*sphere.dynamic_actor);
+	scene.add_actor(sphere);
+	sphere.set_position(0.0f, 10.0f, 10.0f);
 
 	PhysicsObject capsule(create_geometry::capsule(1.0f, 1.0f), true);
-	capsule.position.y = 10.0f;
-	capsule.rotation.x = physx::PxHalfPi;
-	capsule.update_transform();
-	physics_scene->addActor(*capsule.dynamic_actor);
+	scene.add_actor(capsule);
+	sphere.set_position(0.0f, 10.0f, 0.0f);
+	sphere.set_rotation(physx::PxHalfPi, 10.0f, 0.0f);
 
 	std::vector<physx::PxVec3> pyramid_index = { physx::PxVec3(0,1,0), physx::PxVec3(1,0,0), physx::PxVec3(-1,0,0), physx::PxVec3(0,0,1), physx::PxVec3(0,0,-1) };
 	PhysicsObject pyramid(create_geometry::convex_hull(pyramid_index), true);
-	pyramid.position.y = 10.0f;
-	pyramid.position.z = 20.0f;
-	pyramid.update_transform();
-	physics_scene->addActor(*pyramid.dynamic_actor);
+	scene.add_actor(pyramid);
+	pyramid.set_position(0.0f, 10.0f, 20.0f);
 
 	physx::PxMaterial* plane_material = physics->createMaterial(physx::PxReal(0.5f), physx::PxReal(0.5f), physx::PxReal(0.5f));
 	physx::PxRigidStatic* plane_actor = physx::PxCreatePlane(*physics, physx::PxPlane(0.0f, 1.0f, 0.0f, 10.0f), *plane_material);
 	physics_scene->addActor(*plane_actor);
 
-	
-
-
-
 	while (true) {
-		physics_scene->simulate(1 / 1000.0f);
-		physics_scene->fetchResults(true);
-
+		scene.simulation_step_start(1 / 1000.0f);
+		scene.simulation_step_finish();
 	}
 
 }
