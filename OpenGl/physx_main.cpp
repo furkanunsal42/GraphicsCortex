fb9f5811bf878b049e12b018b3edae190e652977
KodBilen42 <89701935+furkanunsal42@users.noreply.github.com>
2022-09-25 19:42:41 +0300

    heightfields are now supported

--- OpenGl/physx_main.cpp
+++ OpenGl/physx_main.cpp
@@ -32,7 +32,7 @@ private:
 		physics_transport = physx::PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
 		physics_pvd->connect(*physics_transport, physx::PxPvdInstrumentationFlag::eALL);
 
-		physics = PxCreateBasePhysics(PX_PHYSICS_VERSION, *physics_foundation, physx::PxTolerancesScale(), true, physics_pvd);
+		physics = PxCreatePhysics(PX_PHYSICS_VERSION, *physics_foundation, physx::PxTolerancesScale(), true, physics_pvd);
 
 		scene_desc = new physx::PxSceneDesc(physics->getTolerancesScale());
 		scene_desc->gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
@@ -100,6 +100,108 @@ namespace create_geometry {
 		return plane(normal.x, normal.y, normal.z, distance);
 	}
 
+	enum heightfield_array_type {
+		ARRAY_OF_ROWS = 0,
+		ARRAY_OF_COLUMNS,
+		ARRAY_OF_ROWS_INVERTED,
+		ARRAY_OF_COLUMNS_INVERTED,
+	};
+
+	physx::PxHeightFieldGeometry heightfield(physx::PxHeightFieldSample heightfield_data[], unsigned int column_size, unsigned int row_size, heightfield_array_type array_type = ARRAY_OF_COLUMNS) {
+		if (column_size == 0 || row_size == 0){
+			std::cout << "[PhysX Error] create_geometry::heightfield() is called but column_size or row_size was zero.\n";
+			ASSERT(false);
+		}
+
+		physx::PxHeightFieldSample* heightfield_data_new = (physx::PxHeightFieldSample*)malloc(sizeof(physx::PxHeightFieldSample) * column_size * row_size);
+		
+		if (array_type == ARRAY_OF_ROWS) {
+			for (int i = 0; i < column_size * row_size; i++) {
+				int column = i / row_size;
+				int row = i % row_size;
+				
+				int i_new = row * column_size + column;
+				heightfield_data_new[i_new] = heightfield_data[i];
+			}
+		}
+		else if (array_type == ARRAY_OF_COLUMNS_INVERTED) {
+			for (int i = 0; i < column_size * row_size; i++) {
+				int column = i / row_size;
+				int row = i % row_size;
+
+				int i_new = (column_size - 1 - column) * row_size + column_size - row - 1;
+				heightfield_data_new[i_new] = heightfield_data[i];
+			}
+		}
+		else if (array_type == ARRAY_OF_ROWS_INVERTED) {
+			for (int i = 0; i < column_size * row_size; i++) {
+				int column = i / row_size;
+				int row = i % row_size;
+
+				int i_new = (row_size - 1 - row) * column_size + row_size - column - 1;
+				heightfield_data_new[i_new] = heightfield_data[i];
+			}
+		}
+
+		delete[] heightfield_data_new;
+
+		physx::PxHeightFieldDesc mesh_desc;
+		//mesh_desc.format = physx::PxHeightFieldFormat::eS16_TM;
+		mesh_desc.nbRows = row_size;
+		mesh_desc.nbColumns = column_size;
+		mesh_desc.samples.stride = sizeof physx::PxHeightFieldSample;
+		
+		if (array_type == ARRAY_OF_COLUMNS)
+			mesh_desc.samples.data = heightfield_data;
+		else
+			mesh_desc.samples.data = heightfield_data_new;
+
+		auto context = PhysxContext::get();
+		physx::PxHeightField* heightfield_mesh = context.physics_cooking->createHeightField(mesh_desc, context.physics->getPhysicsInsertionCallback());
+		/*
+		physx::PxDefaultMemoryOutputStream buf;
+		context.physics_cooking->cookHeightField(mesh_desc, buf);
+		physx::PxDefaultMemoryInputData input(buf.getData(), buf.getSize());
+		physx::PxHeightField* heightfield_mesh = context.physics->createHeightField(input);
+		*/
+		physx::PxMeshGeometryFlags flags;
+
+		return physx::PxHeightFieldGeometry(heightfield_mesh, flags, physx::PxReal(1.0f), physx::PxReal(1.0f), physx::PxReal(1.0f));
+	}
+
+	physx::PxHeightFieldGeometry heightfield(std::vector<physx::PxHeightFieldSample> heightfield_data, unsigned int column_size, unsigned int row_size, heightfield_array_type array_type = ARRAY_OF_COLUMNS) {
+		return heightfield(&(heightfield_data[0]), column_size, row_size, array_type);
+	}
+
+	physx::PxHeightFieldGeometry heightfield(std::vector<std::vector<physx::PxHeightFieldSample>> heightfield_data, heightfield_array_type array_type = ARRAY_OF_COLUMNS) {
+		unsigned int inner_vector_size = heightfield_data[0].size();
+		for (std::vector<physx::PxHeightFieldSample> inner_vector : heightfield_data) {
+			if (inner_vector.size() != inner_vector_size) {
+				std::cout << "[PhysX Error] create_geometry::heightfield() is called but heightfield_data doesn't contain identical sized vectors.\n";
+				ASSERT(false);
+			}
+		}
+		unsigned int outer_vector_size = heightfield_data.size();
+		physx::PxHeightFieldSample* new_data = new physx::PxHeightFieldSample[outer_vector_size * inner_vector_size];
+		for (int i = 0; i < outer_vector_size * inner_vector_size; i++) {
+			int outer = i / inner_vector_size;
+			int inner = i % inner_vector_size;
+
+			new_data[i] = heightfield_data[outer][inner];
+		}
+
+		if (array_type == ARRAY_OF_COLUMNS || array_type == ARRAY_OF_COLUMNS_INVERTED){
+			physx::PxHeightFieldGeometry result = heightfield(&(new_data[0]), inner_vector_size, outer_vector_size, array_type);
+			delete[] new_data;
+			return result;
+		}
+		if (array_type == ARRAY_OF_ROWS || array_type == ARRAY_OF_ROWS_INVERTED){
+			physx::PxHeightFieldGeometry result = heightfield(&(new_data[0]), outer_vector_size, inner_vector_size, array_type);
+			delete[] new_data;
+			return result;
+		}
+	}
+
 	/*
 	physx::PxTriangleMeshGeometry custom(const std::vector<physx::PxVec3>& verticies, const std::vector<unsigned int>& indicies) {
 		auto context = PhysxContext::get();
@@ -141,7 +243,7 @@ public:
 		STATIC,
 	};
 
-	PhysicsObject(const physx::PxGeometry& geometry, unsigned int type = type::DYNAMIC, bool exclusive_shape = false) :
+	PhysicsObject(const physx::PxGeometry& geometry, enum type type = type::DYNAMIC, bool exclusive_shape = false) :
 		position(glm::vec3(0, 0, 0)), rotation(glm::vec3(0, 0, 0)), exclusive_shape(exclusive_shape), type(type)
 	{
 
@@ -275,7 +377,26 @@ int main() {
 
 	PhysicsObject plane(create_geometry::plane(0.0f, 1.0f, 0.0f, 10.0f), true);
 	scene.add_actor(plane);
-	
+
+	std::vector<std::vector<physx::PxHeightFieldSample>> samples;
+	for (int i = 0; i < 5; i++){
+		std::vector<physx::PxHeightFieldSample> sample_inner;
+		for (int j = 0; j < 5; j++) {
+			physx::PxHeightFieldSample sample;
+			if ( j < 2)
+				sample.height = 2;
+			else 
+				sample.height = 0;
+			sample_inner.push_back(sample);
+		}
+		samples.push_back(sample_inner);
+	}
+	PhysicsObject heightfield(create_geometry::heightfield(samples), PhysicsObject::STATIC, true);
+	scene.add_actor(heightfield);
+
+
+
+
 	while (true) {
 		scene.simulation_step_start(1 / 1000.0f);
 		scene.simulation_step_finish();
