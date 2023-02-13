171075e7c881309d8e5ca44c5be8b4a11aeacc8d
Furkan <89701935+KodBilen42@users.noreply.github.com>
2023-02-13 04:19:18 +0300

    Revert "new dynamic ui structure part 1"
    This reverts commit 31d6d9fe6b70e84d9c96f5fb0c5ffc6910b3f3c9.

--- GraphicsCortex/API/Gui.cpp
+++ GraphicsCortex/API/Gui.cpp
@@ -60,188 +60,6 @@ Ui::Ui(Frame& frame) :
 	program = default_program::gui_program();
 }
 
-_widget::_widget(const Ui& ui, const Style& style, Vec2<float> size, Vec2<float> position){
-
-}
-
-void _widget::render(const Vec2<int>& cursor_position){
-	// determine aabbs
-	Vec2<float> screen_position = layouts.back().get_widget_position();
-	Vec2<float> screen_position_hover = screen_position;
-
-	Vec4<float> _style_margin = optional_get<Vec4<float>>({ style.margin });
-	Vec4<float> _style_padding = optional_get<Vec4<float>>({ style.padding });
-
-	Vec4<float> _style_margin_hover = optional_get<Vec4<float>>({ style.on_hover.margin , style.margin });
-	Vec4<float> _style_padding_hover = optional_get<Vec4<float>>({ style.on_hover.padding, style.padding });
-
-	screen_position = screen_position + Vec2<float>(_style_margin.y + _style_padding.y, _style_margin.x + _style_padding.x);
-	screen_position_hover = screen_position_hover + Vec2<float>(_style_margin_hover.y + _style_padding_hover.y, _style_margin_hover.x + _style_padding_hover.x);
-
-	Vec2<float> _size_default = Vec2<float>(size.x - _style_padding.y - _style_padding.w, size.y - _style_padding.x - _style_padding.z);
-	Vec2<float> _size_hover = Vec2<float>(size.x - _style_padding_hover.y - _style_padding_hover.w, size.y - _style_padding_hover.x - _style_padding_hover.z);
-
-	AABB2 default_box(screen_position, _size_default);
-	AABB2 hover_box(screen_position_hover, _size_hover);
-	AABB2 aabb_to_use;
-	Vec2<int> cursor = _ui.frame.get_cursor_position();
-
-	// determine hover
-	bool hover;
-	if (default_box.does_contain(cursor) && hover_box.does_contain(cursor)) {
-		hover = true;
-		_ui._hovered_id = id;
-		_hovered = true;
-		aabb_to_use = hover_box;
-	}
-	else if (default_box.does_contain(cursor)) {
-		hover = true;
-		_hovered_id = id;
-		_hovered = true;
-		aabb_to_use = hover_box;
-	}
-	else if (hover_box.does_contain(cursor)) {
-		if (_hovered_id == id) {
-			hover = true;
-			aabb_to_use = hover_box;
-		}
-		else {
-			hover = false;
-			aabb_to_use = default_box;
-		}
-	}
-	else {
-		hover = false;
-		aabb_to_use = default_box;
-	}
-
-	// hover/active/passive update
-	if (hover) {
-		if ((_cursor_state == Frame::LeftPressed || _cursor_state == Frame::RightPressed) && _cursor_state_just_changed) {
-			_focused_id = id;
-		}
-	}
-
-	// determine style_to_use
-	bool active = _focused_id == id;
-	StaticStyle style_to_use = (StaticStyle)style;
-	if (active) style_to_use = (StaticStyle)style.on_active;
-	else if (hover) style_to_use = (StaticStyle)style.on_hover;
-
-	// insert object to layout
-	Vec3<float> color = optional_get<Vec3<float>>({ style_to_use.color,				style.color });
-	Vec2<float> displacement = optional_get<Vec2<float>>({ style_to_use.displacement,		style.displacement });
-	Vec2<float> rotation_euler = optional_get<Vec2<float>>({ style_to_use.rotation_euler,	style.rotation_euler });
-	Vec4<float> corner_rounding = optional_get<Vec4<float>>({ style_to_use.corner_rounding,	style.corner_rounding });
-	Vec4<float> padding = optional_get<Vec4<float>>({ style_to_use.padding,			style.padding });
-	Vec4<float> margin = optional_get<Vec4<float>>({ style_to_use.margin,			style.margin });
-	Vec4<float> border_thickness = optional_get<Vec4<float>>({ style_to_use.border_thickness,	style.border_thickness });
-	Vec3<float> border_color = optional_get<Vec3<float>>({ style_to_use.border_color,		style.border_color });
-	Frame::CursorType cursor_type = optional_get<Frame::CursorType>({ style_to_use.cursor_type,		style.cursor_type });
-
-	layouts.back().add_widget(Vec2<float>(size.x + margin.y + margin.w, size.y + margin.x + margin.z));
-	Vec2<float> padded_size = size - Vec2<float>(padding.y + padding.w, padding.x + padding.z);
-
-	// render
-	_render_calls.push_back([=]() {
-		if (hover) frame.set_cursor_type(cursor_type);
-
-		Mesh box_mesh(aabb_to_use.generate_model());
-		Mesh_s box_mesh_s = Mesh_s(box_mesh);
-		Program_s program_s = Program_s(program);
-		Graphic box_graphic;
-
-		box_graphic.load_program(program_s);
-		box_graphic.load_model(box_mesh_s);
-		box_graphic.set_uniform("screen_position", screen_position.x, frame.window_height - screen_position.y);
-		box_graphic.set_uniform("projection", projection_matrix);
-		box_graphic.set_uniform("rect_color", color.x, color.y, color.z, 1.0f);
-		box_graphic.set_uniform("rect_size", padded_size.x, padded_size.y);
-		box_graphic.set_uniform("corner_rounding", corner_rounding.x, corner_rounding.y, corner_rounding.z, corner_rounding.w);
-		box_graphic.set_uniform("border_color", border_color.x, border_color.y, border_color.z, 1.0f);
-		box_graphic.set_uniform("border_thickness", border_thickness.x, border_thickness.y, border_thickness.z, border_thickness.w);
-
-
-		box_graphic.update_matrix();
-		box_graphic.update_uniforms();
-		box_graphic.draw(false);
-		});
-}
-
-void _widget::update_position(const Vec2<float>& displacement) {
-	position = position + displacement;
-}
-
-_container::_container(const Ui& ui, const Style& style, _container::LayoutType type, Vec2<float> positinon) :
-	_widget(ui, style, { 0, 0 }, position), type(type) {}
-
-void _container::render(const Vec2<int>& cursor_position){
-	
-	Vec3<float> color = optional_get<Vec3<float>>({ style.color });
-	Vec2<float> displacement = optional_get<Vec2<float>>({ style.displacement });
-	Vec2<float> rotation_euler = optional_get<Vec2<float>>({ style.rotation_euler });
-	Vec4<float> corner_rounding = optional_get<Vec4<float>>({ style.corner_rounding });
-	Vec4<float> padding = optional_get<Vec4<float>>({ style.padding });
-	Vec4<float> margin = optional_get<Vec4<float>>({ style.margin });
-	Vec4<float> border_thickness = optional_get<Vec4<float>>({ style.border_thickness });
-	Vec3<float> border_color = optional_get<Vec3<float>>({ style.border_color });
-	Frame::CursorType cursor_type = optional_get<Frame::CursorType>({ style.cursor_type });
-
-	position = position + Vec2<float>(margin.y + padding.y, margin.x + padding.x);
-	size = Vec2<float>(size.x - padding.y - padding.w, size.y - padding.x - padding.z);
-
-	AABB2 aabb(position, size);
-	Mesh box_mesh(aabb.generate_model());
-	Mesh_s box_mesh_s = Mesh_s(box_mesh);
-	Program_s program_s = Program_s(_ui.program);
-	Graphic box_graphic;
-
-	Vec2<float> padded_size = size - Vec2<float>(padding.y + padding.w, padding.x + padding.z);
-
-	box_graphic.load_program(program_s);
-	box_graphic.load_model(box_mesh_s);
-	box_graphic.set_uniform("screen_position", position.x, _ui.frame.window_height - position.y);
-	box_graphic.set_uniform("projection", _ui.projection_matrix);
-	box_graphic.set_uniform("rect_color", color.x, color.y, color.z, 1.0f);
-	box_graphic.set_uniform("rect_size", size.x, size.y);
-	box_graphic.set_uniform("corner_rounding", corner_rounding.x, corner_rounding.y, corner_rounding.z, corner_rounding.w);
-	box_graphic.set_uniform("border_color", border_color.x, border_color.y, border_color.z, 1.0f);
-	box_graphic.set_uniform("border_thickness", border_thickness.x, border_thickness.y, border_thickness.z, border_thickness.w);
-
-	box_graphic.update_matrix();
-	box_graphic.update_uniforms();
-	box_graphic.draw(false);
-
-	for (_widget& widget : widgets){
-		widget.render(cursor_position);
-		_add_widget(widget.size);
-	}
-}
-
-void _container::update_position(const Vec2<float>& displacement){
-	position = position + displacement;
-}
-
-void _container::_add_widget(const Vec2<float>& widget_size){
-	if (type == Layout::Horizional) {
-		size.x += widget_size.x;
-		size.y = std::max(size.y, widget_size.y);
-	}
-	else if (type == Layout::Vertical) {
-		size.x = std::max(size.x, widget_size.x);
-		size.y += widget_size.y;
-	}
-}
-
-Vec2<float> _container::_get_next_widget_position() {
-	if (type == _container::Horizional) {
-		return Vec2<float>(position.x + size.x, position.y);
-	}
-	else if (type == Layout::Vertical) {
-		return Vec2<float>(position.x, position.y + size.y);
-	}
-}
-
 void Ui::new_frame(){
 	
 	// update time
@@ -273,12 +91,11 @@ void Ui::new_frame(){
 	
 }
 
-void Ui::begin(const Style& style, Layout::LayoutType type){
+void Ui::begin(Layout::LayoutType type){
 	Vec2<float> position;
 	if (layouts.size() > 0)
 		position = layouts.back().get_widget_position();
 	layouts.push_back(Layout(type, position));
-	_layout_styles.push_back(style);
 }
 
 bool Ui::box(const std::string& id, const Vec2<float>& size, const Style& style){
@@ -387,14 +204,11 @@ bool Ui::box(const std::string& id, const Vec2<float>& size, const Style& style)
 
 }
 
-void Ui::end() {
+void Ui::end(const Style& style) {
 	Vec2<float> size = layouts.back().window_size;
 	Vec2<float> position = layouts.back().get_position();
-	
 	// render
 	
-	const Style& style = _layout_styles.back();
-	_layout_styles.pop_back();
 	Vec3<float> color =				optional_get<Vec3<float>>({			style.color				});
 	Vec2<float> displacement =		optional_get<Vec2<float>>({			style.displacement		});
 	Vec2<float> rotation_euler =	optional_get<Vec2<float>>({			style.rotation_euler	});
--- GraphicsCortex/API/Gui.h
+++ GraphicsCortex/API/Gui.h
@@ -1,9 +1,11 @@
 #pragma once
+
 #include "CortexMath.h"
 #include "Frame.h"
 #include "ShaderCompiler.h"
 
 #include <functional>
+#include <chrono>
 #include <optional>
 
 class Layout {
@@ -74,80 +76,24 @@ private:
 	std::function<Style(Time)> _on_passive;
 };
 
-class _widget {
-public:
-	_widget(const Ui& ui, const Style& style, Vec2<float> size = { 0, 0 }, Vec2<float> position = { 0, 0 });
-	Style style;
-	Vec2<float> size;
-	Vec2<float> position;
-	Ui& _ui;
-	virtual void render(const Vec2<int>& cursor_position);
-	virtual void update_position(const Vec2<float>& displacement);
-};
-
-class _container : public _widget {
-public:
-	enum LayoutType {
-		Horizional,
-		Vertical,
-	};
-
-	_container(const Ui& ui, const Style& style, _container::LayoutType type = _container::Horizional, Vec2<float> positinon = {0, 0});
-
-	std::vector<std::reference_wrapper<_widget>> widgets;
-
-	_container::LayoutType type;
-
-	void render(const Vec2<int>& cursor_position);
-	void update_position(const Vec2<float>& displacement);
-	void _add_widget(const Vec2<float>& size);
-	Vec2<float> _get_next_widget_position();
-};
-
-class _text_widget : public _widget {
-public:
-	std::string text;
-};
-
 class Ui {
 public:
 	Ui(Frame& frame);
 	void new_frame();
-
-	// positions and sizes of layouts and inner widgets can change upon hovering,
-	// it creates a compilcated flow for the algorithm.
-	// the algorithm flow for rendering each layout with it's widgets is the follows:
-	// --
-	// compute starting position for layout
-	// compute sizes and positions of widgets (check for hover, if so update)
-	// compute layout size
-	// check layout for hover
-	// if not render layout, render widgets
-	// if so,
-	// compute new position of layout
-	// compute new positions and sizes of widgets (check for hover, if so update)
-	// render layout
-	// render widgets
-	// --
-
-	void begin(const Style& style, Layout::LayoutType type = Layout::Horizional);
+	void begin(Layout::LayoutType type = Layout::Horizional);
 	bool box(const std::string& id, const Vec2<float>& size, const Style& style);
-	void end();
+	void end(const Style& style);
+
 private:
-
-	friend _widget;
-	friend _container;
-
+	
 	Time last_update_ms;
 	Time frame_time;
 	
 	//std::unordered_map<std::string, Time> animation_state;
 	std::vector<std::function<void()>> _render_calls;
-	
 	std::string _focused_id;
 	std::string _hovered_id;
 	std::vector<Layout> layouts;
-	std::vector<Style> _layout_styles;
 	glm::mat4 projection_matrix;
 	Program program;
 	Frame& frame;
