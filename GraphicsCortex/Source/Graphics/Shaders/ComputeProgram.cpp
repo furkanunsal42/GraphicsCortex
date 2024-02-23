34624885a88e9b1b556b87e5acd844f45177ffc0
Furkan <furkan.unsal@tedu.edu.tr>
2024-02-24 00:49:23 +0300

    ComputeShader

--- GraphicsCortex/Source/Graphics/Shaders/ComputeProgram.cpp
+++ GraphicsCortex/Source/Graphics/Shaders/ComputeProgram.cpp
@@ -9,6 +9,7 @@ ComputeProgram::ComputeProgram()
 ComputeProgram::ComputeProgram(const Shader& shader)
 {
 	_generate_program();
+	load_shader(shader);
 }
 
 ComputeProgram::~ComputeProgram()
@@ -21,7 +22,32 @@ void ComputeProgram::release()
 	if (_program_generated) {
 		GLCall(glDeleteProgram(id));
 		_program_generated = false;
+		_program_compiled = false;
 	}
+}
+
+void ComputeProgram::dispatch(int workgroup_size_x, int workgroup_size_y, int workgroup_size_z)
+{
+}
+
+void ComputeProgram::bind()
+{
+	if (!_program_generated) {
+		std::cout << "[OpenGL Error] ComputeProgram tried to bind() but it was released" << std::endl;
+		ASSERT(false);
+	}
+
+	if (!_program_compiled) {
+		std::cout << "[OpenGL Error] ComputeProgram tried to bind() but it wasn't compiled yet" << std::endl;
+		ASSERT(false);
+	}
+
+	GLCall(glUseProgram(id));
+}
+
+void ComputeProgram::unbind()
+{
+	GLCall(glUseProgram(0));
 }
 
 void ComputeProgram::load_shader(const Shader& shader)
@@ -48,7 +74,106 @@ void ComputeProgram::load_shader(const Shader& shader)
 	}
 
 	GLCall(glAttachShader(id, compute_shader));
+	GLCall(glLinkProgram(id));
 
+	int is_linked = 0;
+	GLCall(glGetProgramiv(id, GL_LINK_STATUS, (int*)&is_linked));
+	if (is_linked == GL_FALSE) {
+		GLint max_length = 0;
+		GLCall(glGetProgramiv(id, GL_INFO_LOG_LENGTH, &max_length));
+		std::string info_log(max_length, 0);
+		GLCall(glGetProgramInfoLog(id, max_length, &max_length, &info_log[0]));
+		GLCall(glDeleteProgram(id));
+		GLCall(glDeleteShader(compute_shader));
+
+		std::cout << "[OpenGL Error] Compute Shader \"" << shader.filename << "\" failed to link with message : " << info_log;
+		ASSERT(false);
+		return;
+	}
+
+	GLCall(glDetachShader(id, compute_shader));
+
+	_program_compiled = true;
+}
+
+void ComputeProgram::update_uniform(const std::string& name, Texture2D& texture2d)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, TextureCubeMap& texturecubemap)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const bool& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const bool& a, const bool& b)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const bool& a, const bool& b, const bool& c)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const bool& a, const bool& b, const bool& c, const bool& d)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const int& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const int& a, const int& b)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const int& a, const int& b, const int& c)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const int& a, const int& b, const int& c, const int& d)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const float& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const float& a, const float& b)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const float& a, const float& b, const float& c)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const float& a, const float& b, const float& c, const float& d)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::vec2& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::vec3& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::vec4& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::mat2& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::mat3& a)
+{
+}
+
+void ComputeProgram::update_uniform(const std::string& name, const glm::mat4& a)
+{
 }
 
 void ComputeProgram::_generate_program()
--- GraphicsCortex/Source/Graphics/Shaders/ComputeProgram.h
+++ GraphicsCortex/Source/Graphics/Shaders/ComputeProgram.h
@@ -11,13 +11,42 @@ public:
 	~ComputeProgram();
 	void release();
 
+	void dispatch(int workgroup_size_x, int workgroup_size_y, int workgroup_size_z);
+	void bind();
+	void unbind();
+
 	void load_shader(const Shader& shader);
 
+	void update_uniform(const std::string& name, Texture2D& texture2d);
+	void update_uniform(const std::string& name, TextureCubeMap& texturecubemap);
+	
+	void update_uniform(const std::string& name, const bool& a);
+	void update_uniform(const std::string& name, const bool& a, const bool& b);
+	void update_uniform(const std::string& name, const bool& a, const bool& b, const bool& c);
+	void update_uniform(const std::string& name, const bool& a, const bool& b, const bool& c, const bool& d);
+
+	void update_uniform(const std::string& name, const int& a);
+	void update_uniform(const std::string& name, const int& a, const int& b);
+	void update_uniform(const std::string& name, const int& a, const int& b, const int& c);
+	void update_uniform(const std::string& name, const int& a, const int& b, const int& c, const int& d);
+
+	void update_uniform(const std::string& name, const float& a);
+	void update_uniform(const std::string& name, const float& a, const float& b);
+	void update_uniform(const std::string& name, const float& a, const float& b, const float& c);
+	void update_uniform(const std::string& name, const float& a, const float& b, const float& c, const float& d);
+
+	void update_uniform(const std::string& name, const glm::vec2& a);
+	void update_uniform(const std::string& name, const glm::vec3& a);
+	void update_uniform(const std::string& name, const glm::vec4& a);
+
+	void update_uniform(const std::string& name, const glm::mat2& a);
+	void update_uniform(const std::string& name, const glm::mat3& a);
+	void update_uniform(const std::string& name, const glm::mat4& a);
 
 private:
 
 	bool _program_generated = false;
-
+	bool _program_compiled = false;
 
 	void _generate_program();
 
