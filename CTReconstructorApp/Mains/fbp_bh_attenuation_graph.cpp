80388cb5985397e365e9c01609dfb4b0e23e5abb
Furkan <furkan.unsal@tedu.edu.tr>
2025-01-20 06:54:07 +0300

    bh

--- CTReconstructorApp/Mains/fbp_bh_attenuation_graph.cpp
+++ CTReconstructorApp/Mains/fbp_bh_attenuation_graph.cpp
@@ -4,13 +4,14 @@
 #include "Application/ProgramSourcePaths.h"
 #include "Application/ReconstructionInfo.h"
 #include "DirectoryUtils.h"
+#include "TextureArithmatic/TextureArithmatic.h"
 
 using namespace shader_directory;
 
 
 int main() {
 
-	glm::ivec3 volume_dimentions(1024, 1024, 1024);
+	glm::ivec3 volume_dimentions(512, 512, 512);
 	glm::vec3 voxel_size(200.0f / volume_dimentions.x, 200.0f / volume_dimentions.y, 200.0f / volume_dimentions.z);
 	int projection_count = 1440;
 	int window_width = 1024;
@@ -32,6 +33,7 @@ int main() {
 
 	Texture2D::ColorTextureFormat fbp_volume_format_to_use = floating_point_precision == 16 ? Texture2D::ColorTextureFormat::R16F : Texture2D::ColorTextureFormat::R32F;
 	Texture2D::ColorTextureFormat fbp_projection_format_to_use = floating_point_precision == 16 ? Texture2D::ColorTextureFormat::R16F : Texture2D::ColorTextureFormat::R32F;
+	Texture2D::ColorTextureFormat fbp_distanced_projection_format_to_use = floating_point_precision == 16 ? Texture2D::ColorTextureFormat::RGBA16F : Texture2D::ColorTextureFormat::RGBA32F;
 	Texture2D::ColorTextureFormat fbp_projection_complex_format_to_use = floating_point_precision == 16 ? Texture2D::ColorTextureFormat::RG16F : Texture2D::ColorTextureFormat::RG32F;
 
 	fbp_shader_directory = "../CTReconstructor/Source/GLSL/Compute/FBP/";
@@ -40,10 +42,12 @@ int main() {
 
 	std::shared_ptr<FBP3D> solver = std::make_shared<FBP3D>(fbp_shader_defines_to_use, ffft_shader_defines_to_use);
 	solver->set_projections_max_segment_size(glm::ivec3(volume_dimentions.x, volume_dimentions.y / 1, projection_count));
+	solver->set_distanced_projections_max_segment_size(glm::ivec3(volume_dimentions.x, volume_dimentions.y / 1, projection_count));
 	solver->set_volume_max_segment_size(glm::ivec3(volume_dimentions.x, volume_dimentions.y / 1, volume_dimentions.z));
 
 	solver->set_volume_format(fbp_volume_format_to_use);
 	solver->set_projection_format(fbp_projection_format_to_use);
+	solver->set_distanced_projection_format(fbp_distanced_projection_format_to_use);
 	solver->set_projection_complex_format(fbp_projection_complex_format_to_use);
 
 	solver->set_display_progress(true, 128);
@@ -52,7 +56,7 @@ int main() {
 	min_texture->is_bindless = false;
 	std::shared_ptr<Texture1D> max_texture = std::make_shared<Texture1D>(1, solver->histogram_int_texture_internal_format, 1, 0);
 	max_texture->is_bindless = false;
-	
+
 	solver->read_projections("C:/Users/FurkanPC/Desktop/Projektionen", 2048, 2048, 1, 2, volume_dimentions.x, volume_dimentions.y, projection_count);
 
 	fbp_segmented_memory::iterate_horizontal_projection_segments(*solver, false, true, [&](glm::ivec3 projection_segment_index) {
@@ -61,7 +65,7 @@ int main() {
 		solver->apply_filter_to_projections(FBP2D::FilterType::RAM_LAK);
 		});
 
-	solver->generate_blank_volume(volume_dimentions.x, volume_dimentions.y, volume_dimentions.y);
+	solver->generate_blank_volume(volume_dimentions.x, volume_dimentions.y, volume_dimentions.z);
 
 	fbp_segmented_memory::iterate_horizontal_projection_segments(*solver, true, false, [&](glm::ivec3 projection_index) {
 		fbp_segmented_memory::iterate_horizontal_volume_segments_per_projection(*solver, projection_index.y, [&](glm::ivec3 volume_segment_index) {
@@ -78,9 +82,12 @@ int main() {
 
 	fbp_segmented_memory::iterate_horizontal_volume_segments(*solver, false, false, [&](glm::ivec3 volume_segment_index) {
 		solver->clip_negatives_of_volume();
-		solver->normalize_histogram(*min_texture, *max_texture);
+		//solver->normalize_histogram(*min_texture, *max_texture);
 		});
-	
+
+	solver->generate_blank_distanced_projections(volume_dimentions.x, volume_dimentions.y, volume_dimentions.y/*projection_count*/);
+	solver->bh_project_forward_parallel_to_distanced_projections({ 2, 15 }, 1, volume_dimentions.y/*projection_count*/, 0);
+
 	ReconstructionInfo info;
 	info.name = compute_filename(reconstruction_path);
 	info.volume_resolution = volume_dimentions;
@@ -95,13 +102,16 @@ int main() {
 	slice_complex->is_bindless = false;
 	std::shared_ptr<Texture2D> slice_white = std::make_shared<Texture2D>(volume_dimentions.x, volume_dimentions.z, Texture2D::ColorTextureFormat::RGBA32F, 1, 0, 0);
 	slice_white->is_bindless = false;
+	std::shared_ptr<Texture2D> slice_distanced = std::make_shared<Texture2D>(volume_dimentions.x, volume_dimentions.z, solver->get_distanced_projection_format(), 1, 0, 0);
+	slice->is_bindless = false;
+	TextureArithmatic arithmatic;
 
 	std::shared_ptr<Framebuffer> framebuffer = std::make_shared<Framebuffer>();
 
 	frame.set_visibility(true);
 	while (true) {
 		fbp_segmented_memory::iterate_horizontal_volume_segments(*solver, false, false, [&](glm::ivec3 volume_segment_index) {
-		
+
 			int segment_height = solver->get_volume_max_segment_size().y;
 			for (int slice_id = volume_segment_index.y * segment_height; slice_id < (volume_segment_index.y + 1) * segment_height; slice_id++) {
 				if (!frame.is_running()) exit(0);
@@ -109,11 +119,13 @@ int main() {
 				frame.clear_window();
 				frame.display_performance(180);
 
-				solver->load_volume_slice_y(slice_id, *slice);
+				//solver->load_volume_slice_y(slice_id, *slice);
 				//solver->load_projection(slice_id, *slice);
 				//solver->load_sinogram(slice_id, *slice);
+				solver->load_distanced_projection(slice_id, *slice_distanced);
 
-				solver->fbp_solver->_texture_blit_float1_to_float4(*slice, *slice_white);
+				//solver->fbp_solver->_texture_blit_float1_to_float4(*slice, *slice_white);
+				arithmatic.operation_binary(*slice_white, *slice_distanced, "vec4(operand.xz, 0, 1) / 200");
 
 				framebuffer->attach_color(0, slice_white);
 				framebuffer->set_read_buffer(0);
