3f80e09a2e8dba3b3fae886d2b027828990b2449
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2025-01-21 19:19:50 +0300

    graph rendering test

--- GraphicsCortexApp/Mains/ComputeBufferRenderTest.cpp
+++ GraphicsCortexApp/Mains/ComputeBufferRenderTest.cpp
@@ -9,7 +9,7 @@ int main() {
 	glm::ivec2 resolution(4096, 64);
 	glm::vec2 range_begin(-5, -5);
 	glm::vec2 range_end(5, 5);
-	std::shared_ptr<Buffer> buffer = std::make_shared<Buffer>(6 * sizeof(glm::vec3));
+	std::shared_ptr<Buffer> buffer = std::make_shared<Buffer>(resolution.x * resolution.y * sizeof(glm::vec4));
 	std::shared_ptr<ComputeProgram> kernel = std::make_shared<ComputeProgram>(Shader("../GraphicsCortex/Source/GLSL/Compute/buffer_point_cloud_test.comp"));
 
 	kernel->update_uniform_as_storage_buffer("point_cloud_buffer", *buffer);
@@ -17,15 +17,9 @@ int main() {
 	kernel->update_uniform("range_begin",	glm::vec2(-5, -5));
 	kernel->update_uniform("range_end",		glm::vec2(+5, +5));
 	kernel->update_uniform("amplitude", 1.0f);
-	//kernel->dispatch_thread(resolution.x * resolution.y, 1, 1);
-	std::vector<float> lines_data =
-	{	0, 0, 0, 1, 0, 0,
-		0, 0, 0, 0, 1, 0,
-		0, 0, 0, 0, 0, 1 };
+	kernel->dispatch_thread(resolution.x * resolution.y, 1, 1);
 
-	buffer->load_data(lines_data);
-
-	std::vector<float> data = buffer->get_data<float>(0, 6 * 3);
+	std::vector<float> data = buffer->get_data<float>(0, 1024);
 	for (int i = 0; i < data.size(); i++)
 		std::cout << data[i];
 	std::cout << std::endl;
@@ -35,7 +29,7 @@ int main() {
 	point_cloud_program->update_uniform("model", glm::scale(glm::identity<glm::mat4x4>(), glm::vec3(4, 1.0f / 10, 4)));
 
 	std::shared_ptr<VertexAttributeBuffer> vab = std::make_shared<VertexAttributeBuffer>();
-	vab->attach_vertex_buffer(0, buffer, VertexAttributeBuffer::a_f32, 3, sizeof(glm::vec3), 0, true);
+	vab->attach_vertex_buffer(0, buffer, VertexAttributeBuffer::a_f32, 3, sizeof(glm::vec4), 0, true);
 
 	float angle = 3.14 / 4;
 
@@ -53,6 +47,12 @@ int main() {
 		glm::mat4x4 model_matrix = glm::scale(glm::rotate(glm::identity<glm::mat4x4>(), angle, glm::vec3(0, 1, 0)), glm::vec3(1.0f / resolution.x, 1.0f / 1, 1.0f / resolution.y));
 		point_cloud_program->update_uniform("model", model_matrix);
 		
-		primitive_renderer::render(*point_cloud_program, *vab, PrimitiveType::line);
+		primitive_renderer::render(*point_cloud_program, *vab, PrimitiveType::line_strip);
+	
+		model_matrix = glm::scale(glm::rotate(glm::identity<glm::mat4x4>(), angle + 3.14f/2, glm::vec3(0, 1, 0)), glm::vec3(1.0f / resolution.x, 1.0f / 1, 1.0f / resolution.y));
+		point_cloud_program->update_uniform("model", model_matrix);
+
+		primitive_renderer::render(*point_cloud_program, *vab, PrimitiveType::line_strip);
+
 	}
 }
