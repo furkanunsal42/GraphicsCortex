b3d476c0320d796dbf971d7a439244436d72d35f
Furkan <furkan.unsal@tedu.edu.tr>
2025-02-25 07:26:40 +0300

    asset loading tests

--- GraphicsCortexApp/Tests/Model/ModelLoadingTest.h
+++ GraphicsCortexApp/Tests/Model/ModelLoadingTest.h
@@ -13,13 +13,23 @@ public:
 		//Asset asset("../GraphicsCortex/Models/boxes/boxes.obj");
 		//Asset asset("../GraphicsCortex/Models/medival/medival.fbx");
 		//Asset asset("../GraphicsCortex/Models/sculpture/scene.gltf");
-		Asset asset("../GraphicsCortex/Models/dragon.obj");
+		//Asset asset("../GraphicsCortex/Models/dragon.obj");
+		//Asset asset("../GraphicsCortex/Models/Thinker/Rodin_Thinker.obj");
+		//Asset asset("../GraphicsCortex/Models/porsche_chassis.obj");
+		Asset asset("../GraphicsCortex/Models/teducar/teduCar.fbx");
+
 		Model model = asset.load_model();
 		Mesh mesh(model);
 
 		std::shared_ptr<Program> program_flat = default_program::flatcolor_program_s();
-		program_flat->update_uniform("model", glm::mat4(1));
-		program_flat->update_uniform("color", glm::vec4(1));
+		std::shared_ptr<Program> program_uv = std::make_shared<Program>(
+			Shader(
+				"../GraphicsCortex/Source/GLSL/Debug/basic.vert",
+				"../GraphicsCortex/Source/GLSL/Debug/texcoord.frag")
+		);
+
+		program_uv->update_uniform("model", glm::mat4(1));
+		program_uv->update_uniform("color", glm::vec4(1));
 
 		Camera camera;
 		camera.mouse_sensitivity = 10;
@@ -36,14 +46,23 @@ public:
 
 			camera.handle_movements((GLFWwindow*)default_window->get_handle(), deltatime);
 			camera.update_matrixes();
-			camera.update_default_uniforms(*program_flat);
+			camera.update_default_uniforms(*program_uv);
 
 			mesh[0].traverse([&](Mesh::Node& node, glm::mat4& transform) {
 
+				glm::vec4 pos4 = transform * glm::vec4(0, 1, 0, 1);
+				glm::vec3 pos3 = glm::vec3(pos4) / pos4.w;
+
+				std::cout << node.get_name() << std::endl;
+				std::cout << pos3.x << " " << pos3.y << " " << pos3.z << std::endl;
+
+				//glEnable(GL_CULL_FACE);
+				glEnable(GL_DEPTH_TEST);
+
 				for (mesh_t submesh : node.get_submeshes()) {
-					program_flat->update_uniform("model", transform);
+					program_uv->update_uniform("model", transform);
 					primitive_renderer::render(
-						*program_flat,
+						*program_uv,
 						*mesh.get_mesh(submesh)
 					);
 				}
