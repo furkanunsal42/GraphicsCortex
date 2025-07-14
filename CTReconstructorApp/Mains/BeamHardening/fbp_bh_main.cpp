967d1224ac9cf70684bb6b26e101ef6a45a1c1a3
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2025-07-14 19:05:43 +0300

    main

--- CTReconstructorApp/Mains/BeamHardening/fbp_bh_main.cpp
+++ CTReconstructorApp/Mains/BeamHardening/fbp_bh_main.cpp
@@ -2,16 +2,6 @@
 
 int main() {
 	
-	int32_t window_width = 1024;
-	WindowDescription desc;
-	desc.w_resolution = glm::ivec2(window_width);
-	desc.w_name = "CTReconstructor";
-	desc.w_scale_framebuffer_size = false;
-	desc.w_scale_window_size = false;
-	desc.w_visible = false;
-	
-	Window window(desc);
-
 	// pipe
 	//std::filesystem::path descriptor_file_path		= "C:/Users/furkan.unsal/Desktop/Data2/rekonstruktion.ini";
 	//std::filesystem::path projections_path			= "C:/Users/furkan.unsal/Desktop/Data2/projektion";
@@ -23,9 +13,9 @@ int main() {
 	//std::filesystem::path volume_path			= "C:/Users/furkan.unsal/Desktop/CTReconstruction2";
 
 	// sage
-	std::filesystem::path descriptor_file_path = "C:/Users/furkan.unsal/Desktop/deneme_21_03_2023.txt";
-	std::filesystem::path projections_path = "C:/Users/furkan.unsal/Desktop/Projektionen";
-	std::filesystem::path volume_path = "C:/Users/furkan.unsal/Desktop/CTReconstruction3";
+	std::filesystem::path descriptor_file_path	= "C:/Users/furkan.unsal/Desktop/deneme_21_03_2023.txt";
+	std::filesystem::path projections_path		= "C:/Users/furkan.unsal/Desktop/Projektionen";
+	std::filesystem::path volume_path			= "C:/Users/furkan.unsal/Desktop/CTReconstruction3";
 
 	ct_reconstructor::init();
 
@@ -47,11 +37,18 @@ int main() {
 	);
 
 	solver.read_projections(parameters);
-	ct_reconstructor::back_project(solver, geometry, parameters, CTOption::transfer_inputs_from_ram_on_begin);
+	ct_reconstructor::back_project(solver, geometry, parameters,
+		transfer_inputs_from_ram_on_begin |
+		apply_filter_to_projections |
+		apply_log_normalization_to_projections
+		//apply_minmax_normalization_to_volume |
+		//clip_negatives_of_volume
+	);
 
 	solver.generate_blank_distanced_projections(parameters.volume_resolution.x, parameters.volume_resolution.y, parameters.volume_resolution.y/*projection_count*/, 2);
-	solver.bh_project_forward_parallel_to_distanced_projections({ 2, 15, 30, 100 }, 1, parameters.volume_resolution.y/*projection_count*/, 0);
+	solver.bh_project_forward_parallel_to_distanced_projections({ 2, 11, 50, 100 }, 1, parameters.volume_resolution.y/*projection_count*/, 0);
 
+	
 	//solver->project_forward_parallel(1, projection_count, 0);
 
 	std::shared_ptr<Texture2D> slice = std::make_shared<Texture2D>(parameters.volume_resolution.x, parameters.volume_resolution.z, solver.get_volume_format(), 1, 0, 0);
@@ -61,32 +58,36 @@ int main() {
 
 	std::shared_ptr<Framebuffer> framebuffer = std::make_shared<Framebuffer>();
 
-	window.set_window_visibility(true);
+	ct_reconstructor::context->set_window_visibility(true);
 	while (true) {
 		fbp_segmented_memory::iterate_horizontal_volume_segments(solver, false, false, [&](glm::ivec3 volume_segment_index) {
 
 			int segment_height = solver.get_volume_max_segment_size().y;
 			for (int slice_id = volume_segment_index.y * segment_height; slice_id < (volume_segment_index.y + 1) * segment_height; slice_id++) {
-				if (window.should_close()) exit(0);
-				double delta_time = window.handle_events(true);
+				if (ct_reconstructor::context->should_close()) {
+					ct_reconstructor::release();
+					exit(0);
+				}
+				double delta_time = ct_reconstructor::context->handle_events(true);
 				primitive_renderer::clear();
 
-				solver.load_volume_slice_y(slice_id, *slice);
+				//solver.load_volume_slice_y(slice_id, *slice);
 				//solver.load_projection(slice_id, *slice);
 				//solver.load_sinogram(slice_id, *slice);
-				//solver.load_distanced_projection(slice_id, 0, *slice_distanced);
+				solver.load_distanced_projection(slice_id, 1, *slice_distanced);
 
-				//solver->fbp_solver->_texture_blit_float1_to_float4(*slice, *slice_white);
-
+				//solver.fbp_solver->_texture_blit_float1_to_float4(*slice, *slice_white);
 				//framebuffer->attach_color(0, slice_white);
 				framebuffer->attach_color(0, slice_distanced);
 
 				framebuffer->set_read_buffer(0);
-				framebuffer->blit_to_screen(0, 0, parameters.volume_resolution.x, parameters.volume_resolution.z, 0, 0, window_width, window_width, Framebuffer::Channel::COLOR, Framebuffer::Filter::LINEAR);
+				framebuffer->blit_to_screen(0, 0, parameters.volume_resolution.x, parameters.volume_resolution.z, 0, 0, ct_reconstructor::context->get_window_resolution().x, ct_reconstructor::context->get_window_resolution().y, Framebuffer::Channel::COLOR, Framebuffer::Filter::LINEAR);
 				
-				window.swap_buffers();
+				ct_reconstructor::context->swap_buffers();
 				std::this_thread::sleep_for(std::chrono::milliseconds(16));
 			}
 			});
 	}
+
+	ct_reconstructor::release();
 }
