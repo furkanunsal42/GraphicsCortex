3865b1d55ecb39e9dd46401c607b880db6ed417d
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2025-08-26 15:03:19 +0300

    fft rework part 2

--- CTReconstructorApp/Mains/FFT/fft_main.cpp
+++ CTReconstructorApp/Mains/FFT/fft_main.cpp
@@ -8,32 +8,30 @@ int main() {
 	desc.w_resolution = glm::ivec2(2);
 	Window window(desc);
 
-	glm::ivec2 texture_resolution = glm::ivec2(600);
+	glm::ivec2 texture_resolution = glm::ivec2(1024);
 
 	Image image("../CTReconstructor/Images/lenna.png", 4, true);
 	Texture2D texture = Texture2D(texture_resolution.x, texture_resolution.y, Texture2D::ColorTextureFormat::R16F, 1, 0, 0);
 	
-	image.resize(texture_resolution.x, texture_resolution.y);
-	texture.load_data(image, Texture2D::ColorFormat::RGBA, Texture2D::Type::UNSIGNED_BYTE, 0);
+	//image.resize(texture_resolution.x, texture_resolution.y);
+	//texture.load_data(image, Texture2D::ColorFormat::RGBA, Texture2D::Type::UNSIGNED_BYTE, 0);
 
 	GraphicsOperation op;
 	
-	//op.push_constant("texture_resolution", texture_resolution);
-	//op.push_constant("frequency", glm::vec2(42, 0));
-	//
-	//op.compute(
-	//	texture,
-	//	"if (id.x >= 800)"
-	//	"	imageStore(target_data, ivec2(id.xy), vec4(0));"
-	//	"else"
-	//	"	imageStore(target_data, ivec2(id.xy), vec4(cos((id.x / float(texture_resolution.x) * frequency.x + id.y / float(texture_resolution.y) * frequency.y) * 2 * 3.14159265358979323846)));"
-	//	""
-	//);
+	op.push_constant("texture_resolution", texture_resolution);
+	op.push_constant("frequency", glm::vec2(12, 0));
+	
+	op.compute(
+		texture,
+		"imageStore(target_data, ivec2(id.xy), vec4(cos((id.x / float(texture_resolution.x) * frequency.x + id.y / float(texture_resolution.y) * frequency.y) * 2 * 3.14159265358979323846)));"
+	);
 
 	FFFT fft_solver;
 
 	std::shared_ptr<Texture2D> complex_texture = fft_solver.create_complex_texture_from_real(texture);
-	std::shared_ptr<Texture2D> padded_complex_texture = fft_solver.create_padded_complex_texture(*complex_texture);
+	std::shared_ptr<Texture2D> complex_texture2 = complex_texture->create_texture_with_same_parameters();
+
+	//std::shared_ptr<Texture2D> padded_complex_texture = fft_solver.create_padded_complex_texture(*complex_texture);
 
 	//auto signal_image = padded_complex_texture->get_image(Texture2D::ColorFormat::RG, Texture2D::Type::FLOAT, 0, 0, 0, padded_complex_texture->get_size().x, 1);
 	//float* signal = (float*)signal_image->get_image_data();
@@ -48,6 +46,9 @@ int main() {
 
 	//fft_solver.fft(*padded_complex_texture, *padded_complex_texture);
 	
+	fft_solver.fft(*complex_texture);
+	//fft_solver.inverse_dft(*complex_texture);
+
 	//op.clear();
 	//op.push_constant("sample_count", padded_complex_texture->get_size().x);
 	//op.compute(
@@ -80,33 +81,54 @@ int main() {
 	//	}
 	//}
 
-	for (int32_t y = 0; y < complex_texture->get_size().y; y++) {
+	//for (int32_t y = 0; y < complex_texture->get_size().y; y++) {
+	//
+	//	auto signal_image = complex_texture->get_image(Texture2D::ColorFormat::RG, Texture2D::Type::FLOAT, 0, 0, y, complex_texture->get_size().x, 1);
+	//	float* signal = (float*)signal_image->get_image_data();
+	//	std::vector<FFT::complex> signal_v;
+	//	for (int32_t i = 0; i < complex_texture->get_size().x; i++)
+	//		signal_v.push_back(FFT::complex(signal[i * 2 + 0], signal[i * 2 + 1]));
+	//
+	//	std::vector<FFT::complex> temp;
+	//	FFT::dft(signal_v, temp);
+	//	FFT::inverse_dft(temp, signal_v);
+	//
+	//	for (int32_t i = 0; i < complex_texture->get_size().x; i++) {
+	//		signal[2 * i + 0] = signal_v[i].r;
+	//		signal[2 * i + 1] = signal_v[i].i;
+	//	}
+	//
+	//	complex_texture->load_data(signal, Texture2D::ColorFormat::RG, Texture2D::Type::FLOAT, 0, y, complex_texture->get_size().x, 1);
+	//
+	//	if (y == 0) {
+	//		std::ofstream f("cosf42_cortex.txt");
+	//		for (int32_t i = 0; i < signal_v.size(); i++)
+	//			f << "(" << signal_v[i].r << ", " << signal_v[i].i << ")" << std::endl;
+	//		f.close();
+	//	}
+	//}
 
-		auto signal_image = complex_texture->get_image(Texture2D::ColorFormat::RG, Texture2D::Type::FLOAT, 0, 0, y, complex_texture->get_size().x, 1);
-		float* signal = (float*)signal_image->get_image_data();
-		std::vector<FFT::complex> signal_v;
-		for (int32_t i = 0; i < complex_texture->get_size().x; i++)
-			signal_v.push_back(FFT::complex(signal[i * 2 + 0], signal[i * 2 + 1]));
-
-		std::vector<FFT::complex> temp;
-		FFT::dft(signal_v, temp);
-		FFT::inverse_dft(temp, signal_v);
-
-		for (int32_t i = 0; i < complex_texture->get_size().x; i++) {
-			signal[2 * i + 0] = signal_v[i].r;
-			signal[2 * i + 1] = signal_v[i].i;
-		}
-
-		complex_texture->load_data(signal, Texture2D::ColorFormat::RG, Texture2D::Type::FLOAT, 0, y, complex_texture->get_size().x, 1);
-
-		if (y == 0) {
-			std::ofstream f("cosf42_cortex.txt");
-			for (int32_t i = 0; i < signal_v.size(); i++)
-				f << "(" << signal_v[i].r << ", " << signal_v[i].i << ")" << std::endl;
-			f.close();
-		}
-	}
-
+	//for (int32_t y = 0; y < complex_texture->get_size().y; y++) {
+	//
+	//	auto signal_image = texture.get_image(Texture2D::ColorFormat::RED, Texture2D::Type::FLOAT, 0, 0, y, texture.get_size().x, 1);
+	//	float* signal = (float*)signal_image->get_image_data();
+	//	std::vector<float> signal_v;
+	//	for (int32_t i = 0; i < complex_texture->get_size().x; i++)
+	//		signal_v.push_back(signal[i]);
+	//
+	//	std::vector<FFT::complex> temp;
+	//	FFT::dft_real(signal_v, temp);
+	//	FFT::inverse_dft_real(temp, signal_v);
+	//
+	//	texture.load_data(signal_v.data(), Texture2D::ColorFormat::RED, Texture2D::Type::FLOAT, 0, y, texture.get_size().x, 1);
+	//
+	//	if (y == 0) {
+	//		std::ofstream f("cosf42_cortex.txt");
+	//		for (int32_t i = 0; i < signal_v.size(); i++)
+	//			f << "(" << temp[i].r << ", " << temp[i].i << ")" << std::endl;
+	//		f.close();
+	//	}
+	//}
 
 	//fft_solver.fft_shift(*padded_complex_texture);
 	//
@@ -120,7 +142,7 @@ int main() {
 	while (!window.should_close()) {
 		window.handle_events(true);
 		primitive_renderer::clear(0, 0, 0, 0);
-		
+
 		window.set_window_resolution(complex_texture->get_size());
 		fb.blit_to_screen(complex_texture->get_size(), window.get_framebuffer_resolution(), Framebuffer::Channel::COLOR, Framebuffer::Filter::LINEAR);
 
