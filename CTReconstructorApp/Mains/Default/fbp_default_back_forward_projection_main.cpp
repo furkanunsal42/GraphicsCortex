36ff49b67aab7e9ba3a5f9635e1b62171f9d206a
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2025-09-08 19:00:29 +0300

    forward projection rework

---
+++ CTReconstructorApp/Mains/Default/fbp_default_back_forward_projection_main.cpp
@@ -0,0 +1,82 @@
+#include "CTReconstructor.h"
+
+int main() {
+	std::filesystem::path working_directory = std::filesystem::absolute(".");
+	working_directory = "C:\\Users\\furkan.unsal\\Desktop\\drive\\20250716111737.002\\[vg-data] 20250716111737.002";
+
+	//ct_reconstructor::init_from_package();
+	ct_reconstructor::init();
+
+	ParameterParser parser;
+	ASSERT(parser.read(working_directory));
+	parser.parameters.filter_type = FBP2D::RAM_LAK;
+	
+	parser.parameters.input_data_type = FBP3D::Projections;
+	parser.parameters.output_data_type = FBP3D::Volume;
+	parser.geometry.detector_plane_offset.x = 0;
+	parser.geometry.detector_plane_offset.y = 0;
+	parser.geometry.rotation_plane_offset_x = 0;
+	parser.geometry.source_detector_tilt_x_radian = 0;
+	parser.geometry.rotation_radian_for_each_scan = glm::pi<float>() * 2;
+
+	FBP3D solver(parser);
+
+	solver.generate_blank_volume(parser.parameters);
+	solver.render_sphere_to_volume(glm::vec3(0), 0.5, 0.5, false);
+
+	ct_reconstructor::launch_debug_window(solver);
+
+	parser.parameters.input_data_type = FBP3D::Volume;
+	parser.parameters.output_data_type = FBP3D::Projections;
+
+	ct_reconstructor::forward_project(solver, parser,
+		0
+	);
+
+	ct_reconstructor::launch_debug_window(solver);
+
+	parser.parameters.input_data_type = FBP3D::Projections;
+	parser.parameters.output_data_type = FBP3D::Volume;
+
+	ct_reconstructor::filter(solver, parser,
+		//transfer_inputs_from_ram_on_begin |
+		//apply_log_normalization_to_projections |
+		apply_filter_to_projections
+	);
+	
+	ct_reconstructor::launch_debug_window(solver);
+
+	//solver.generate_blank_volume(parser.parameters);
+	//bool canceled = !ct_reconstructor::launch_preview_window(solver, parser);
+	//if (canceled) {
+	//	ct_reconstructor::release();
+	//	return 0;
+	//}
+
+	ct_reconstructor::back_project(solver, parser,
+		0
+		//apply_minmax_normalization_to_volume |
+		//clip_negatives_of_volume |
+		//save_output_to_disk
+	);
+	fbp_segmented_memory::iterate_horizontal_volume_segments(solver, false, false, [&](glm::ivec3 volume_segment_index) {
+		solver.render_cylinder_to_volume(glm::vec3(0), glm::vec3(0, 1, 0), 1, 0, true);
+		});
+
+
+	ct_reconstructor::launch_debug_window(solver);
+	
+	//parser.parameters.input_data_type = FBP3D::Volume;
+	//parser.parameters.output_data_type = FBP3D::Projections;
+	//parser.parameters.input_resolution = glm::ivec3(1000);
+	//parser.parameters.output_resolution = glm::ivec3(1000);
+	//
+	//ct_reconstructor::forward_project(solver, parser,
+	//	0
+	//);
+
+
+	ct_reconstructor::launch_debug_window(solver);
+
+	ct_reconstructor::release();
+}
--- CTReconstructorApp/Mains/Default/fbp_default_forward_projection_main.cpp
+++ CTReconstructorApp/Mains/Default/fbp_default_forward_projection_main.cpp
@@ -2,44 +2,30 @@
 
 int main() {
 
-	// pipe
-	//std::filesystem::path descriptor_file_path		= "C:/Users/furkan.unsal/Desktop/Data2/scaninfo.ini";
+	std::filesystem::path working_directory = std::filesystem::absolute(".");
+	working_directory = "C:\\Users\\furkan.unsal\\Desktop\\Data3\\[vg-data]20240802111906.478";
 
-	// pen
-	//std::filesystem::path descriptor_file_path	= "C:/Users/FurkanPC/Desktop/Data3/[vg-data]20240802111906.478/scaninfo.ini";
-
-	// sage
-	//std::filesystem::path descriptor_file_path	= "C:/Users/furkan.unsal/Desktop/deneme_21_03_2023.txt";
-	//std::filesystem::path projections_path		= "C:/Users/furkan.unsal/Desktop/Projektionen";
-	//std::filesystem::path volume_path			= "C:/Users/furkan.unsal/Desktop/CTReconstruction3/CTReconstruction47";
-
-	// demlik (tiltless)
-	//std::filesystem::path descriptor_file_path = "C:/Users/FurkanPC/Desktop/drive/new_data/Demlik/[vg-data] 20250818145043.850";
-
-	// bardak (tiltless)
-	//std::filesystem::path descriptor_file_path = "C:/Users/FurkanPC/Desktop/drive/new_data/Bardak tilt 0/[vg-data] 20250818135056.904";
-
-	// xbox (tiltless)
-	std::filesystem::path descriptor_file_path = "C:/Users/FurkanPC/Desktop/drive/new_data/xbox/[vg-data] 20250818150439.488";
-
-	ct_reconstructor::init();
+	ct_reconstructor::init_from_package();
 
 	ParameterParser parser;
-	ASSERT(parser.read(descriptor_file_path));
+	ASSERT(parser.read(working_directory, 0));
 
-	parser.geometry.detector_plane_offset.x = 0;
-	parser.geometry.detector_plane_offset.y = 0;
-	parser.geometry.detector_plane_tilt_radian = glm::radians(00.0f);
-	parser.geometry.rotation_plane_offset_x = 0;
-	parser.geometry.source_detector_tilt_x_radian = glm::radians(80.0f);
+	parser.parameters.input_data_type = FBP3D::Volume;
+	parser.parameters.output_data_type = FBP3D::Projections;
+	parser.parameters.input_resolution = glm::ivec3(1024);
+	parser.parameters.output_resolution = glm::ivec3(1024);
+	parser.validify();
+	parser.print();
 
 	FBP3D solver(parser);
 
 	solver.generate_blank_volume(parser);
 	fbp_segmented_memory::iterate_horizontal_volume_segments(solver, false, false, [&](glm::ivec3 volume_segment_index) {
-		solver.generate_shepplogan();
-		//solver.render_sphere(glm::vec3(0), 1, 1);
+		//solver.generate_shepplogan();
+		solver.render_sphere(glm::vec3(0), 10, 1);
 		});
+	
+	ct_reconstructor::launch_debug_window(solver);
 
 	ct_reconstructor::forward_project(solver, parser, 0);
 
